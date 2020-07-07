3eed2b727a61267374e583fabeca3f651311dcce
Marcos G. Zimmermann <mgzmaster@gmail.com>
2020-07-07 12:13:08 -0300

    Use attr_reader for getter methods

--- Dice.rb
+++ Dice.rb
@@ -1,14 +1,12 @@
 class Dice
+  attr_reader :dice_limit
+
   def initialize(dice_limit = 6)
     @dice_limit = dice_limit
   end
 
   def roll
     rand(1..@dice_limit)
-  end
-
-  def dice_limit
-    @dice_limit
   end
 
   def dice_range
